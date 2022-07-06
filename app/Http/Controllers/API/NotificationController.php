fcd33214a9f31c28ac266373cd24158dafe0922d
Leonardo Kriswanto <leon12okto@gmail.com>
2022-07-06 12:44:18 +0700

    Update NotificationController.php

--- app/Http/Controllers/API/NotificationController.php
+++ app/Http/Controllers/API/NotificationController.php
@@ -5,23 +5,29 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class NotificationController extends Controller
 {
 
     public function send(Request $request)
     {
-        $response = Http::withHeaders([
-            'Content-Type' => 'application/json',
-            'Authorization' => 'key=AAAA7NpuTF0:APA91bGoZNzN0veBBz6e9dX8BSGlOrbzlsmyoNLVQ4SCm4m_bv7RYswZ38kzSWWi9VCtthYWIxWLaVHHRZmA41ypwt6YOX4AXx2OrKWzR5YZ3ELsy-RBOl4xRax0-80GqP0Yr66J8dPy'
-        ])->post('https://fcm.googleapis.com/fcm/send', [
-            'to' => '/topics/hrm' . strtolower(auth()->user()->name),
-            'data' => [
-                'title' => 'Perhatian',
-                'message' => 'Kontak anda ' . auth()->user()->name . ' terdeteksi mendapatkan detak jantung melampaui batas yang ditetapkan!'
-            ]
-        ]);
+        $contacts = auth()->user()->contacts;
+        $contact_arr = preg_split ("/\,/", $contacts);
+        foreach($contact_arr as $contact) {
+            $data = User::where('user_id', $contact);
+            $name = $data->name;
+            $response = Http::withHeaders([
+                'Content-Type' => 'application/json',
+                'Authorization' => 'key=AAAA7NpuTF0:APA91bGoZNzN0veBBz6e9dX8BSGlOrbzlsmyoNLVQ4SCm4m_bv7RYswZ38kzSWWi9VCtthYWIxWLaVHHRZmA41ypwt6YOX4AXx2OrKWzR5YZ3ELsy-RBOl4xRax0-80GqP0Yr66J8dPy'
+            ])->post('https://fcm.googleapis.com/fcm/send', [
+                'to' => '/topics/hrm' . strtolower(auth()->user()->name),
+                'data' => [
+                    'title' => 'Perhatian',
+                    'message' => 'Kontak anda ' . $name . ' terdeteksi mendapatkan detak jantung melampaui batas yang ditetapkan!'
+                ]
+            ]);
+        }
 
         return response()->json(['success' => true, 'message' => $response]);
     }
