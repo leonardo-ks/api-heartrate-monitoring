26ad07633d9857af99a81ec22106fd1d0d09d35d
Leonardo Kriswanto <41584357+leonardo-ks@users.noreply.github.com>
2022-07-19 19:20:35 +0700

    Update NotificationController.php

--- app/Http/Controllers/API/NotificationController.php
+++ app/Http/Controllers/API/NotificationController.php
@@ -26,14 +26,14 @@ class NotificationController extends Controller
                 $message = "Detak jantung ". $name ." lebih tinggi saat bergerak";
             } else if($request->status == 4) {
                 $message = "Detak jantung ". $name ." lebih tinggi saat bergerak";
-            } else {
+            } else if($request->status == 5) {
                 $message = $name ." menekan tombol darurat, segera berikan tindakan!";
             }
             $response = Http::withHeaders([
                 'Content-Type' => 'application/json',
                 'Authorization' => 'key=AAAA7NpuTF0:APA91bGoZNzN0veBBz6e9dX8BSGlOrbzlsmyoNLVQ4SCm4m_bv7RYswZ38kzSWWi9VCtthYWIxWLaVHHRZmA41ypwt6YOX4AXx2OrKWzR5YZ3ELsy-RBOl4xRax0-80GqP0Yr66J8dPy'
             ])->post('https://fcm.googleapis.com/fcm/send', [
-                'to' => '/topics/hrmadmin' . strtolower($name),
+                'to' => '/topics/hrm' . strtolower($name),
                 'data' => [
                     'title' => 'Perhatian',
                     'message' => $message,
