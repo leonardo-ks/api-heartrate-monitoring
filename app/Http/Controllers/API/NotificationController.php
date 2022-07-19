1d0d427cfe23f5dbd4e2643550cead5c14926677
Leonardo Kriswanto <41584357+leonardo-ks@users.noreply.github.com>
2022-07-19 20:39:03 +0700

    Update NotificationController.php

--- app/Http/Controllers/API/NotificationController.php
+++ app/Http/Controllers/API/NotificationController.php
@@ -42,7 +42,6 @@ class NotificationController extends Controller
         }
 
         $decoded = json_decode($response->getBody()->getContents(), true);
-        dd($name);
 
         if(array_key_exists("message_id",$decoded)) {
             return response()->json(['success' => true]);
